279f9ec4122e293a0b064f6b1f435ca6b8bcd789
guidoschaap43 <g.schaap@dhd.nl>
2025-02-14 11:59:13 +0100

    updated paths to setup.exe and officeconfig.xms

--- office.ps1
+++ office.ps1
@@ -1,7 +1,7 @@
 New-Item -Path C:\ -Name "Temp" -ItemType Directory
 
-Invoke-WebRequest -Uri 'https://stravdsystem.blob.core.windows.net/aib/Installers/setup.exe' -OutFile 'c:\temp\setup.exe'
-Invoke-WebRequest -Uri 'https://stravdsystem.blob.core.windows.net/aib/Installers/OfficeConfig.xml' -OutFile 'c:\temp\officeconfig.xml'
+Invoke-WebRequest -Uri 'https://github.com/guidoschaap43/aib/blob/9dcb930572419b8328fa3584a6926c96f8c1200f/setup.exe' -OutFile 'c:\temp\setup.exe'
+Invoke-WebRequest -Uri 'https://github.com/guidoschaap43/aib/blob/9dcb930572419b8328fa3584a6926c96f8c1200f/officeconfig.xml' -OutFile 'c:\temp\officeconfig.xml'
 
 Start-Sleep -Seconds 10
 
