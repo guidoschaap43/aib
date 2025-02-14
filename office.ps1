77b683b14b63e3b0528b1e1d9fdf71939cf67cd6
guidoschaap43 <g.schaap@dhd.nl>
2025-02-14 12:42:08 +0100

    Added write-host

--- office.ps1
+++ office.ps1
@@ -1,9 +1,16 @@
+Write-host "Creating temp directory"
 New-Item -Path C:\ -Name "Temp" -ItemType Directory
 
+write-host "Downloading setup and config"
 Invoke-WebRequest -Uri 'https://github.com/guidoschaap43/aib/blob/9dcb930572419b8328fa3584a6926c96f8c1200f/setup.exe' -OutFile 'c:\temp\setup.exe'
 Invoke-WebRequest -Uri 'https://github.com/guidoschaap43/aib/blob/9dcb930572419b8328fa3584a6926c96f8c1200f/officeconfig.xml' -OutFile 'c:\temp\officeconfig.xml'
 
 Start-Sleep -Seconds 10
 
+write-host "Downloading office"
 Start-Process -Wait -FilePath C:\temp\setup.exe -ArgumentList "/download c:\temp\officeconfig.xml"
+
+write-host "Installing office"
 Start-Process -Wait -FilePath C:\temp\setup.exe -ArgumentList "/configure c:\temp\officeconfig.xml"
+
+write-host "script finished"
